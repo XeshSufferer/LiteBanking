b708644d59624e1838798358fe7916bf1070a02a
ByXesh <3a4em_yt@mail.ru>
2025-09-27 19:24:23 +0200

    FixModels

--- Models/Domain/Balance.cs
+++ Models/Domain/Balance.cs
@@ -1,6 +1,15 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace LiteBanking.Models.Domain;
+
 
 public class Balance
 {
+    public long Id { get; set; }
+    public long OwnerId { get; set; }
+    public User Owner { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     
 }
--- Models/Domain/User.cs
+++ Models/Domain/User.cs
@@ -1,6 +1,14 @@
+using LiteBanking.EFCoreFiles;
+using LiteBanking.Сache;
+using Microsoft.EntityFrameworkCore;
+
 namespace LiteBanking.Models.Domain;
 
 public class User
 {
-    
+    public long Id { get; set; }
+    public string PhoneNumber { get; set; } = default!;
+    public string HashKeyRecoveryWord { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
 }
