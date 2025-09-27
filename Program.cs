f19697999908989e769de784c769a8848b03ca72
ByXesh <3a4em_yt@mail.ru>
2025-09-27 19:13:13 +0200

    RegisterRepositoryInDI

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using System.Text.Json.Serialization.Metadata;
 using LiteBanking.Сache;
 using System.Security.Claims;
 using System.Text;
+using LiteBanking.EFCoreFiles;
+using LiteBanking.Repositories;
+using LiteBanking.Repositories.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,8 +16,18 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.InstanceName = "cache";
 });
 
+builder.Services.AddScoped<AppDbContext>();
+
 builder.Services.AddSingleton<ICacheService, RedisService>();
 
+builder.Services.AddScoped<IBalanceRepository, BalanceRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+
 var app = builder.Build();
+
+
+
 
 app.Run();
