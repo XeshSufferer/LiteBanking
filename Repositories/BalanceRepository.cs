4902fa1c5cec43c6a41c53c1c2fd0059bb9cb24d
ByXesh <3a4em_yt@mail.ru>
2025-09-27 19:10:02 +0200

    AddRepositories x2

--- Repositories/BalanceRepository.cs
+++ Repositories/BalanceRepository.cs
@@ -1,6 +1,54 @@
+using LiteBanking.EFCoreFiles;
+using LiteBanking.Models.Domain;
+using LiteBanking.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace LiteBanking.Repositories;
 
-public class BalanceRepository
+public class BalanceRepository : IBalanceRepository
 {
+    
+    private readonly AppDbContext _context;
+    
+    public BalanceRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> CreateBalance(Balance balance, CancellationToken ct = default)
+    {
+        try
+        {
+            await _context.Balances.AddAsync(balance, ct);
+            await _context.SaveChangesAsync(ct);
+            return true;
+        }
+        catch (DbUpdateException e)
+        {
+            return false;
+        }
+    }
+
+    public async Task<Balance?> GetBalanceById(long id, CancellationToken ct = default) => 
+        await _context.Balances.Where(b => b.Id == id).FirstOrDefaultAsync(ct);
+
+    public async Task<bool> UpdateBalance(Balance balance, CancellationToken ct = default)
+    {
+        try
+        {
+            _context.Balances.Update(balance);
+            await _context.SaveChangesAsync(ct);
+            return true;
+        }
+        catch (DbUpdateException e)
+        {
+            return false;
+        }
+    }
+
+    public async Task<List<Balance>?> GetAllUserBalances(long userId, CancellationToken ct = default) =>
+        await _context.Balances.Where(b => b.OwnerId == userId).ToListAsync(ct);
+    
+    
     
 }
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,6 +1,73 @@
+using System.Data.Common;
+using LiteBanking.EFCoreFiles;
+using LiteBanking.Models.Domain;
+using LiteBanking.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace LiteBanking.Repositories;
 
-public class UserRepository
+public class UserRepository : IUserRepository
 {
-    
+
+    private readonly AppDbContext _context;
+    private readonly ILogger<UserRepository> _logger;
+
+    public UserRepository(AppDbContext dbContext, ILogger<UserRepository> logger)
+    {
+        _context = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<bool> CreateUser(User user, CancellationToken ct = default)
+    {
+        try
+        {
+            await _context.Users.AddAsync(user, ct);
+            await _context.SaveChangesAsync(ct);
+            return true;
+        }
+        catch (DbUpdateException e)
+        {
+            _logger.LogError("Unable to create user {number}", user.PhoneNumber);
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteUser(long id, CancellationToken ct = default)
+    {
+        try
+        {
+            await _context.Users.Where(u => u.Id == id).ExecuteDeleteAsync(ct);
+            return true;
+        }
+        catch (DbUpdateException e)
+        {
+            _logger.LogError("Unable to delete user {id}", id);
+            return false;
+        }
+    }
+
+    public async Task<bool> UpdateUser(User user, CancellationToken ct = default)
+    {
+        try
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync(ct);
+            return true;
+        }
+        catch (DbUpdateException e)
+        {
+            _logger.LogError("Unable to update user {user}", user.PhoneNumber);
+            return false;
+        }
+    }
+
+    public async Task<User?> GetUserById(long id, CancellationToken ct = default) => 
+        await _context.Users.Where(u => u.Id == id).FirstOrDefaultAsync(ct);
+
+    public async Task<User?> GetUserByPhoneNumber(string number, CancellationToken ct = default) =>
+        await _context.Users.Where(u => u.PhoneNumber == number).FirstOrDefaultAsync(ct);
+
+    public async Task<User?> GetUserByUsername(string username, CancellationToken ct = default) =>
+        await _context.Users.Where(u => u.Name == username).FirstOrDefaultAsync(ct);
 }
